2f8f8d330a76fb6cba703eb2b1d1f52bf5bf27ed
Jimmy <jimmyzhang1@hotmail.com>
2022-11-28 16:43:38 -0800

    Added /url page with a table, constructed with ejs

--- express_server.js
+++ express_server.js
@@ -18,9 +18,14 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render('urls_index', templateVars);
+});
+
 //Sending HTML
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.send("<html><body>Helloooooooooo <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
