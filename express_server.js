bedfeadb7d3da9db31554ec30e684e13bc26cb29
Jimmy <jimmyzhang1@hotmail.com>
2022-11-30 17:34:35 -0800

    Added separate login/logout pages that can be accessed in the header.

--- express_server.js
+++ express_server.js
@@ -10,21 +10,48 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+const users = {
+  jimmyzhng: {
+    id: "jimmyzhng",
+    email: "jimmyzhang1@hotmail.com",
+    password: "123450",
+  }
+};
+
+// Random URL generator
 const generateRandomString = function() {
   return Math.random().toString(36).slice(2, 8);
 };
 
+// Check if email exists
+const getUserByEmail = (email, users) => {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return user;
+    }
+  }
+  return false;
+};
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
-  next(); // runs the next callback
+  next();
 });
 
+// Homepage
 app.get('/', (req, res) => {
+  const templateVars = {
+    users
+  };
+  console.log(templateVars);
+  console.log(req.cookies);
   res.send('hello!');
 });
 
+// URL Stuff
 // this shows the JSON string representing the entire obj
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
@@ -33,23 +60,23 @@ app.get('/urls.json', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies["username"]
+    users,
+    cookies: req.cookies
   };
 
   res.render('urls_index', templateVars);
 });
 
 app.post("/urls", (req, res) => {
-
   const key = generateRandomString();
-
   urlDatabase[key] = req.body.longURL;
   res.redirect(`/urls/${key}`);
 });
 
 app.get('/urls/new', (req, res) => {
   const templateVars = {
-    username: req.cookies["username"]
+    users,
+    cookies: req.cookies
   };
   res.render('urls_new', templateVars);
 });
@@ -58,7 +85,8 @@ app.get('/urls/:id', (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"]
+    users,
+    cookies: req.cookies
   };
   res.render('urls_show', templateVars);
 });
@@ -81,15 +109,62 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+// Login/Logout
+app.get('/login', (req, res) => {
+  const templateVars = {
+    users,
+    cookies: req.cookies
+  };
+  res.render('urls_login', templateVars);
+});
+
 app.post("/login", (req, res) => {
-  // console.log(req.body);
-  res.cookie('username', req.body.username);
+  const email = req.body.email;
+  const password = req.body.password;
+  const user = getUserByEmail(email, users);
+
+  if (!user) {
+    return res.status(400).send('Sorry, that account does not exist!');
+  }
+
+  if (password !== users[user].password) {
+    res.status(403).send('Sorry, that password is invalid. You have 1 more try, or you will be IP banned from our website.');
+  }
+
+  res.cookie('user_id', user);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username', req.cookies["username"]);
-  res.redirect("/urls");
+  res.clearCookie('user_id', req.cookies["user_id"]);
+  res.redirect("/login");
+});
+
+// Register
+app.get('/register', (req, res) => {
+  const templateVars = {
+    users,
+    cookies: req.cookies
+  };
+  res.render('urls_register', templateVars);
+});
+
+app.post('/register', (req, res) => {
+  const userId = generateRandomString();
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(400).send('Sorry, please enter a valid email and password.');
+  }
+
+  if (getUserByEmail(email, users)) {
+    return res.status(400).send('Sorry, that account already exists!');
+  }
+
+  users[userId] = { userId, email, password };
+  res.cookie('user_id', userId);
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
